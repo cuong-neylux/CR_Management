d20b43a8381edc79b880dabbe88a54d7bd7f7d96
Cuong Luc Dinh <dluc@neylux.com>
2022-11-08 13:00:14 +0000

    changed a bit in controller files

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -1,9 +1,19 @@
 sap.ui.define([],
 function (){
     "use strict";
+
+    // Global varables
+    var crNum;
+    var oModel;
+
     return {
         onInit: function(){
-            alert('onInit is called!');
+            oModel = this.getOwnerComponent().getModel();
+
+            this.extensionAPI.attachPageDataLoaded(function (oEvent){
+                crNum = this.getView().byId("NYX.bsincrv01::sap.suite.ui.generic.template.ObjectPage.view.Details::CR_HeaderSet--header::headerEditable::op_cr_general_info::CrNum::Field").getValue();
+                sap.ui.controller("NYX.bsincrv01.ext.controller.Timeline").loadMessages(oModel, crNum);
+            }.bind(this));
         },
         onSubmitClick: function(oEvent) {
             alert('onSubmitClick');
