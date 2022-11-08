16057b1ed9459cc514e3c991659dd2287c8d1722
Cuong Luc Dinh <dluc@neylux.com>
2022-11-08 12:16:46 +0000

    moved CR number to general information, so that controller can access it with id

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -2,6 +2,9 @@ sap.ui.define([],
 function (){
     "use strict";
     return {
+        onInit: function(){
+            alert('onInit is called!');
+        },
         onSubmitClick: function(oEvent) {
             alert('onSubmitClick');
         }
