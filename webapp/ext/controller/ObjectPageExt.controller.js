ab818fb35758d19a72b558cfb0c8aae47332a97f
Cuong Luc Dinh <dluc@neylux.com>
2022-11-08 15:23:04 +0000

    fixed the message history

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -5,14 +5,16 @@ function (){
     // Global varables
     var crNum;
     var oModel;
+    var that;
 
     return {
         onInit: function(){
             oModel = this.getOwnerComponent().getModel();
+            that = this;
 
             this.extensionAPI.attachPageDataLoaded(function (oEvent){
                 crNum = this.getView().byId("NYX.bsincrv01::sap.suite.ui.generic.template.ObjectPage.view.Details::CR_HeaderSet--header::headerEditable::op_cr_general_info::CrNum::Field").getValue();
-                sap.ui.controller("NYX.bsincrv01.ext.controller.Timeline").loadMessages(oModel, crNum);
+                sap.ui.controller("NYX.bsincrv01.ext.controller.Timeline").loadMessages(that, oModel, crNum);
             }.bind(this));
         },
         onSubmitClick: function(oEvent) {
--- webapp/ext/controller/Timeline.controller.js
+++ webapp/ext/controller/Timeline.controller.js
@@ -2,42 +2,49 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", "sap
 	function (Controller, JSONModel, MessageToast) {
 		"use strict";
 
-		var sText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nmaximus magna eget tempor iaculis. Aliquam erat volutpat. Morbi sodales laoreet augue ac pellentesque. In facilisis at nisl non bibendum. Cras mattis bibendum libero elementum condimentum. Quisque ac finibus elit. Nam molestie eget nisl pulvinar rutrum. Sed in vulputate lorem, convallis sodales quam. Sed condimentum turpis a feugiat commodo. Nulla finibus nec risus non tempus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ex tortor, viverra sit amet tortor ut, placerat placerat turpis. Vivamus sapien mauris, semper vel ante ut, vestibulum pharetra orci." +
-			"Curabitur vulputate nisi vel efficitur dapibus. Cras eleifend eget dui non condimentum. Aliquam sit amet eros mi. Aliquam erat volutpat. Donec rhoncus ante ut mauris tempor, sit amet pharetra urna congue. Morbi semper ullamcorper lectus a fringilla. Curabitur ac mi in risus ultrices malesuada vel quis arcu. Aenean sed nisl et augue consequat commodo. Praesent sapien enim, faucibus eget nisi in, pretium maximus lacus. Duis pulvinar dapibus neque in tristique. Integer ut neque sed enim mollis convallis. Proin nisi dolor, varius a turpis maximus, molestie suscipit enim. Praesent rutrum venenatis facilisis. Vivamus vel quam at erat aliquet ultricies. Ut aliquet semper auctor." +
-			"In cursus, lorem eget convallis sodales, ante risus finibus augue, at euismod dui enim at nisl. Donec maximus, ipsum at egestas tempus, nisi urna lacinia leo, at tempor velit magna vel dui. Pellentesque fermentum auctor quam, non vehicula felis elementum nec. Quisque eget libero at dolor tincidunt imperdiet. Praesent mi mi, faucibus a neque sit amet, mollis vulputate est. Vivamus mattis facilisis nisi et tincidunt. Nam bibendum aliquet iaculis. In odio metus, semper ac placerat quis, cursus sed dolor. Mauris scelerisque sem id porta varius. Maecenas odio diam, egestas eget leo eu, sagittis convallis erat. Donec quam metus, interdum sit amet molestie ut, interdum vel arcu. Nullam dolor justo, semper convallis mollis sed, hendrerit id quam. Proin non aliquet tortor, non facilisis nibh. Praesent ut lorem tellus. Donec tincidunt condimentum condimentum. Aenean tempor lacus vel odio ultricies tempus." +
-			"Donec eu lorem varius eros suscipit egestas. Etiam vel risus odio. Sed lobortis nisi vitae sodales tempor. Suspendisse ut sapien nec diam maximus convallis. Cras eu nunc aliquam, malesuada quam at, congue purus. Donec nec augue non nisi egestas rutrum id vitae justo. Donec vestibulum venenatis dui ac posuere. Aenean et enim volutpat, dignissim velit eget, tempor mauris. Sed lobortis augue ac purus blandit vestibulum. Maecenas ut bibendum diam. In a placerat nunc, et semper arcu. Quisque sollicitudin, purus sit amet efficitur vestibulum, augue urna tincidunt lectus, vitae pulvinar lorem justo id ex. Vivamus at massa arcu. Proin sapien lorem, ultrices a ullamcorper eu, tincidunt ac mi. Nam eu vulputate dui, id elementum metus. Proin finibus neque nulla, eget mattis nisi rutrum non.";
 		var oModel;
+		var messages;
+		var that;
 		return Controller.extend("sap.suite.ui.commons.sample.TimelineTextHeight.Timeline", {
 
-			loadMessages: function (pModel, crNum) {
+			loadMessages: function (globalController, pModel, crNum) {
+				that = this;
 				oModel = pModel;
-				oModel.read("/CommentSet(CrNum='" + crNum + "')", {
+				var filter = new sap.ui.model.Filter("CrNum", sap.ui.model.FilterOperator.EQ, crNum + '"');
+				var aFilters = [filter];
+				oModel.read("/CommentSet", {
+					filters: aFilters,
 					success: function (oData) {
+						messages = oData.results;
+						that.displayMessages(globalController);
 						console.log("Load messages:", oData);
 					},
 					error: function (error) {
 						console.log("Load messages:", error.Text);
+						var messageHistorySection = this.getView.byId("NYX.bsincrv01::sap.suite.ui.generic.template.ObjectPage.view.Details::CR_HeaderSet--AfterFacet::CR_HeaderSet::op_section_docList::Section");
+						messageHistorySection.setVisible(false);
 					}
 				});
+
+			},
+			displayMessages: function (globalController){
 				var aData = {
 					Items: []
 				};
 
 				var oDate = new Date();
-				for (var i = 1; i < 150; i++) {
+				for (var i = 0; i < messages.length; i++) {
 					aData.Items.push({
-						Date: new Date(new Date(oDate).setDate(oDate.getDate() - i)),
+						Date: new Date(messages[i].CommentDate),
 						Title: i + "",
-						Text: sText.substring(0, Math.random() * 1500),
-						UserName: 'User'
+						Text: messages[i].Note,
+						UserName: 'User'//messages[i].FirstName + ', ' + messages[i].Surname
 					});
 				}
 
-				//var oModel = new JSONModel(aData);
-
-				//oModel.setSizeLimit(150);
-				//this.getView().setModel(oModel);
-				this._timeline = this.byId("idTimeline");
+				var lModel = new JSONModel(aData);
+				globalController.getView().setModel(lModel);
+				globalController._timeline = this.byId("idTimeline");
 			},
 			onPressItems: function (evt) {
 				MessageToast.show("The TimelineItem is pressed.");
