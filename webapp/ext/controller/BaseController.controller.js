0dd3e30b8d6d17db35f43b634633d038f15d7ef7
Cuong Luc Dinh <dluc@neylux.com>
2022-11-09 09:42:58 +0000

    changed the default value of panal in timeline

---
+++ webapp/ext/controller/BaseController.controller.js
@@ -0,0 +1,65 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("myCompany.myApp.controller.BaseController", {
+		/**
+		 * Convenience method for accessing the router.
+		 * @public
+		 * @returns {sap.ui.core.routing.Router} the router for this component
+		 */
+		getRouter: function() {
+			return sap.ui.core.UIComponent.getRouterFor(this);
+		},
+
+        /**
+		 * Convenience method for setting the controller.
+		 * @public
+		 * @param {sap.ui.model.Controller} controller the controller instance
+		 */
+		setController: function(controller) {
+			this = controller;
+		},
+        		/**
+		 * Convenience method for getting the view model by name.
+		 * @public
+		 * @returns {sap.ui.model.Controller} the controller instance
+		 */
+		getController: function() {
+			return this;
+		},
+
+		/**
+		 * Convenience method for getting the view model by name.
+		 * @public
+		 * @param {string} [sName] the model name
+		 * @returns {sap.ui.model.Model} the model instance
+		 */
+		getModel: function(sName) {
+			return this.getView().getModel(sName);
+		},
+
+		/**
+		 * Convenience method for setting the view model.
+		 * @public
+		 * @param {sap.ui.model.Model} oModel the model instance
+		 * @param {string} sName the model name
+		 * @returns {sap.ui.mvc.View} the view instance
+		 */
+		setModel: function(oModel, sName) {
+			return this.getView().setModel(oModel, sName);
+		},
+
+		/**
+		 * Getter for the resource bundle.
+		 * @public
+		 * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
+		 */
+		getResourceBundle: function() {
+			return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+		},
+
+	});
+
+});
--- webapp/ext/controller/Timeline.controller.js
+++ webapp/ext/controller/Timeline.controller.js
@@ -2,17 +2,17 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", "sap
 	function (Controller, JSONModel, MessageToast) {
 		"use strict";
 
-		var oModel;
+		var gModel;
 		var messages;
 		var that;
 		return Controller.extend("sap.suite.ui.commons.sample.TimelineTextHeight.Timeline", {
 
-			loadMessages: function (globalController, pModel, crNum) {
+			loadMessages: function (globalController, globalModel, crNum) {
 				that = this;
-				oModel = pModel;
+				gModel = globalModel;
 				var filter = new sap.ui.model.Filter("CrNum", sap.ui.model.FilterOperator.EQ, crNum + '"');
 				var aFilters = [filter];
-				oModel.read("/CommentSet", {
+				gModel.read("/CommentSet", {
 					filters: aFilters,
 					success: function (oData) {
 						messages = oData.results;
@@ -32,10 +32,12 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", "sap
 					Items: []
 				};
 
-				var oDate = new Date();
+				var oDate;
 				for (var i = 0; i < messages.length; i++) {
+					oDate = new Date(messages[i].CommentDate);
+					oDate.setTime(oDate.getTime() + messages[i].CommentTime)						// Subtract one hour
 					aData.Items.push({
-						Date: new Date(messages[i].CommentDate),
+						Date: oDate,
 						Title: i + "",
 						Text: messages[i].Note,
 						UserName: 'User'//messages[i].FirstName + ', ' + messages[i].Surname
@@ -43,12 +45,10 @@ sap.ui.define(["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel", "sap
 				}
 
 				var lModel = new JSONModel(aData);
-				globalController.getView().setModel(lModel);
+				globalController.getView().setModel(lModel, "timelineView");
 				globalController._timeline = this.byId("idTimeline");
 			},
-			onPressItems: function (evt) {
-				MessageToast.show("The TimelineItem is pressed.");
-			},
+
 			sliderChanged: function (oEvent) {
 				this.byId("idPanel").setHeight(oEvent.getParameter("value") + "px");
 			}
