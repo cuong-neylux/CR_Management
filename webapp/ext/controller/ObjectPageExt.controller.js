1ce5d6eafe3f5c613da25455e33313187b12fb48
Cuong Luc Dinh <dluc@neylux.com>
2022-11-11 15:46:40 +0000

    added TaskSet in manifest file and added a field for photoURL, the task list is not loaded automatically

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -13,8 +13,6 @@ sap.ui.define(["./BaseController", "sap/ui/core/mvc/Controller"],
                 // Attach a handler to the PageDataLoaded event. This event is fired each time the object page is navigated to or the object to be displayed is changed Note.
                 this.extensionAPI.attachPageDataLoaded(function (oEvent) {
                     gModel = this.getOwnerComponent().getModel();
-                    crNum = this.getView().byId("NYX.bsincrv01::sap.suite.ui.generic.template.ObjectPage.view.Details::CR_HeaderSet--header::headerEditable::op_cr_general_info::CrNum::Field").getValue();
-                    //sap.ui.controller("NYX.bsincrv01.ext.controller.Timeline").loadMessages(gController, gModel, crNum);
                 }.bind(this));
             },
             onSubmitClick: function (oEvent) {
