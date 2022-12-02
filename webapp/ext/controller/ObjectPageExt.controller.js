8f08f9c50d66ef006c221d6ec3081367ec02fc08
Cuong Luc Dinh <dluc@neylux.com>
2022-12-02 14:22:44 +0000

    added i18n for english, get Collection back but it doesn't load DocumentSet yet

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -1,23 +1,27 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"],
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/upload/UploadSetItem"],
     function () {
         "use strict";
 
         return {
-            onInit: function () {
+            onInit: function (Controller, UploadSetItem) {
                 var oPromise;
                 // Attach a handler to the PageDataLoaded event. This event is fired each time the object page is navigated to or the object to be displayed is changed Note.
                 this.extensionAPI.attachPageDataLoaded(function (oEvent) {
+                    console.log("OP: Page Loaded!");
                     var crObject = oEvent.context.sPath;
-                    //Store current A1mnr and Pernr in a model
+                    //Store current CrNum in model
                     var crNum = this.getOwnerComponent().getModel().getProperty(crObject).CrNum;
-                    this.getView().getModel("crNum").setData(crNum);    
+                    this.getView().getModel("crNum").setData(crNum);
+
                 }.bind(this));
             },
-            onBeforeRendering: function(oEvent){
-
+            onBeforeRendering: function (oEvent) {
+                console.log("OP: onBeforeRendering!");
             },
-            onAfterRendering: function(oEvent){
-
+            onAfterRendering: function (oEvent) {
+                console.log("OP: onAfterRendering!");
                 this.extensionAPI.getTransactionController().attachAfterSave(async function (oEvent) {
                     sap.ui.controller("NYX.bsincrv01.ext.controller.TaskTable").test();
                 });
--- webapp/ext/controller/UploadSet.controller.js
+++ webapp/ext/controller/UploadSet.controller.js
@@ -3,9 +3,8 @@ sap.ui.define([
 	"sap/m/library",
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/core/Item",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/core/CustomData"
-], function (jQuery, MobileLibrary, Controller, Item, JSONModel, CustomData) {
+	"sap/ui/model/json/JSONModel"
+], function (jQuery, MobileLibrary, Controller, Item, JSONModel) {
 	"use strict";
 
 	return Controller.extend("NYX.bsincrv01.ext.controller.UploadSet", {
@@ -14,15 +13,40 @@ sap.ui.define([
 			var sPath = "../mockdata" + "/items.json";
 			this.getView().setModel(new JSONModel(sPath));
 
+			var oModel = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/nyx/BS_IN_CR_GP01_V01_SRV/", true);
 			var oUploadSet = this.byId("UploadSet");
 			oUploadSet.getList().setMode(MobileLibrary.ListMode.MultiSelect);
 
 			// Modify "add file" button
 			oUploadSet.getDefaultFileUploader().setButtonOnly(false);
 			oUploadSet.getDefaultFileUploader().setIcon("sap-icon://attachment");
+
+/* 			var oModel = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/nyx/BS_IN_CR_GP01_V01_SRV/", true);
+			var oUploadSet = this.byId("UploadSet");
+			var oUploadItem = this.byId("UploadSet-UploadSetItem");
+			oModel.read("/DocumentSet",
+				{
+					success: function (oData) {
+						debugger;
+						var documents = oData.results;
+						documents.forEach(element => {
+							oUploadItem.setFileName(element.FileName);
+							oUploadItem.setMediaType(element.MimeType);
+							oUploadItem.setUrl(element.Url);
+							oUploadSet.addItem(oUploadItem);
+						});
+						
+					},
+					error: function (error) {
+
+					}
+				});
+			oModel.refresh; */
 		},
 		onAfterRendering: function () {
+			console.log("UploadSet: onAfterRendering!");
 			var oUploadSet = this.byId("UploadSet");
+
 			oUploadSet.attachAfterItemAdded(function (oEvent) {
 				var oItem = oEvent.mParameters.item;
 				this.addIncompleteItem(oItem);
@@ -56,7 +80,7 @@ sap.ui.define([
 				}
 			});
 
-			oUploadSet.attachUploadCompleted(function (oEvent){
+			oUploadSet.attachUploadCompleted(function (oEvent) {
 
 			});
 		},
